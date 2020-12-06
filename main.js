805af37397736f4f6c6d48e840d43d9b3b6a1a72
simon2669 <feltalalo26@gmail.com>
2020-12-06 15:04:22 +0100

    kártyák elhelyezése

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const card = document.querySelectorAll('.card');
+
+const back = document.querySelectorAll('.back img');
+const front = document.querySelectorAll('.front img');
+(function () {
+  front.forEach((item) => item.src = './pictures/cow.jpg')
+})();
+
+
 (function () {
   card.forEach((item) => item.addEventListener('click', () => item.classList.toggle('sajt')))
 })();
@@ -14,13 +22,37 @@ function randomChooseFromZeroToFour(arr) {
     let randomNumber = arr[Math.floor(Math.random() * arr.length)];
     szamok.unshift(randomNumber)
     arr.splice(arr.indexOf(randomNumber), 1)
-
-
   }
-  console.log(szamok)
-
+  //console.log(szamok)
+  return szamok;
 }
-randomChooseFromZeroToFour(zeroToFour)
-randomChooseFromZeroToFour(zeroToNien)
+const nullaKilenc = randomChooseFromZeroToFour(zeroToNien)
+const nullaNégy = randomChooseFromZeroToFour(zeroToFour);
+function givePicturesToCards() {
+  let szám = 0;
+  let index = 0;
+  let hozzáadotKártya = 0;
+  const számokNégyig = nullaNégy;
+  let kártyaPár;
+
+  while (hozzáadotKártya < 10) {
+    kártyaPár = számokNégyig[index];
+    if (szám < 2) {
+      //back[hozzáadotKártya].src = './pictures/0.jpg';
+      back[hozzáadotKártya].src = `./pictures/${kártyaPár}.jpg`;
+      szám++;
+      hozzáadotKártya++;
+
+    } else {
+      console.log('else')
+      szám = 0;
+      index++;
+
+    }
+  }
+}
+
+givePicturesToCards();
+
 
 
